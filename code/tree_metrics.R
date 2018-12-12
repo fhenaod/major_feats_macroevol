4168640a2910ef58ef00243d35ea9bb567fa3fff
Francisco Henao Diaz <luisfranciscohenaodiaz@MacBook-Pro-de-Francisco.local>
2018-12-11 18:49:02 -0800

    basic tree metrics

---
+++ code/tree_metrics.R
@@ -0,0 +1,19 @@
+library(ape)
+
+tree_metrics=function(tt){
+  ntips<-c()
+  tree.min.age<-c()
+  tree.max.age<-c()
+  gamma.stat<-c()
+  trees.metrics<-c()
+  for (i in 1:length(tt)){
+    phylogs<-tt[[i]]
+    ntips[i]<-Ntip(phylogs)
+    tree.min.age[i]<-min(branching.times(phylogs))
+    tree.max.age[i]<-max(branching.times(phylogs))
+    gamma.stat[i]<-gammaStat(phylogs)
+    trees.metrics<-data.frame(ntips,tree.min.age,tree.max.age,gamma.stat)
+  }  
+  return(trees.metrics)
+}
+tree_metrics(e.trees)

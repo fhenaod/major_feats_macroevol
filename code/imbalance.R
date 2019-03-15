dce718fa4b142dfa013b3de5c5d0ec61a244af2c
Francisco Henao Diaz <franciscohenaodiaz@dhcp-206-87-221-251.ubcsecure.wireless.ubc.ca>
2019-03-14 22:14:43 -0700

    fixed  imbalance function to avoid crash in trees with less that 4 tips

--- code/imbalance.R
+++ code/imbalance.R
@@ -33,30 +33,47 @@ imbalance_metrics=function(e.trees,n.mc){
   imbalance_metrics<-c()
   for (i in 1:length(e.trees)){
     tr<-e.trees[[i]]
-    tree.ts<-as.treeshape(tr)
     
-    shape.yule[i]<-shape.statistic(tree.ts, norm = "yule")
-    colles.yule[i]<-colless(tree.ts, norm = "yule")
-    sackin.yule[i]<-sackin(tree.ts, norm = "yule")
-    
-    shape.pda[i]<-shape.statistic(tree.ts, norm = "pda")
-    colles.pda[i]<-colless(tree.ts,norm = "pda")
-    sackin.pda[i]<-sackin(tree.ts, norm = "pda")
-    
-    df<-data.frame(colless.test(tree.ts, model = "yule", alternative = "less", n.mc = n.mc))
-    p.coless.t.y.less[i]<-df$p.value
-    df1<-data.frame(colless.test(tree.ts, model = "yule", alternative = "greater", n.mc = n.mc))
-    p.coless.t.y.great[i]<-df1$p.value
-    df2<-data.frame(likelihood.test(tree.ts, model = "yule", alternative = "two.sided"))
-    p.lt.yule[i]<-df2$p.value
-    
-    df3<-data.frame(colless.test(tree.ts, model = "pda", alternative = "less", n.mc = n.mc))
-    p.coless.t.pda.less[i]<-df3$p.value
-    df4<-data.frame(colless.test(tree.ts, model = "pda", alternative = "greater", n.mc = n.mc))
-    p.coless.t.pda.great[i]<-df4$p.value
-    df5<-data.frame(likelihood.test(tree.ts, model = "pda", alternative = "two.sided"))
-    p.lt.pda[i]<-df5$p.value
-     
+    if (length(tr$tip.label)<= 4) {
+      shape.yule[i]<-NA
+      colles.yule[i]<-NA
+      p.coless.t.y.less[i]<-NA
+      p.coless.t.y.great[i]<-NA
+      sackin.yule[i]<-NA
+      p.lt.yule[i]<-NA
+      shape.pda[i]<-NA
+      colles.pda[i]<-NA
+      p.coless.t.pda.less[i]<-NA
+      p.coless.t.pda.great[i]<-NA
+      sackin.pda[i]<-NA
+      p.lt.pda[i]<-NA
+    } else 
+      {
+      tree.ts<-as.treeshape(tr)
+      
+      shape.yule[i]<-shape.statistic(tree.ts, norm = "yule")
+      colles.yule[i]<-colless(tree.ts, norm = "yule")
+      sackin.yule[i]<-sackin(tree.ts, norm = "yule")
+      
+      shape.pda[i]<-shape.statistic(tree.ts, norm = "pda")
+      colles.pda[i]<-colless(tree.ts,norm = "pda")
+      sackin.pda[i]<-sackin(tree.ts, norm = "pda")
+      
+      df<-data.frame(colless.test(tree.ts, model = "yule", alternative = "less", n.mc = n.mc))
+      p.coless.t.y.less[i]<-df$p.value
+      df1<-data.frame(colless.test(tree.ts, model = "yule", alternative = "greater", n.mc = n.mc))
+      p.coless.t.y.great[i]<-df1$p.value
+      df2<-data.frame(likelihood.test(tree.ts, model = "yule", alternative = "two.sided"))
+      p.lt.yule[i]<-df2$p.value
+      
+      df3<-data.frame(colless.test(tree.ts, model = "pda", alternative = "less", n.mc = n.mc))
+      p.coless.t.pda.less[i]<-df3$p.value
+      df4<-data.frame(colless.test(tree.ts, model = "pda", alternative = "greater", n.mc = n.mc))
+      p.coless.t.pda.great[i]<-df4$p.value
+      df5<-data.frame(likelihood.test(tree.ts, model = "pda", alternative = "two.sided"))
+      p.lt.pda[i]<-df5$p.value
+    }
+
 imbalance.metrics<-data.frame(shape.yule,colles.yule,p.coless.t.y.less,p.coless.t.y.great,sackin.yule,
   p.lt.yule,shape.pda,colles.pda,p.coless.t.pda.less,p.coless.t.pda.great,sackin.pda,p.lt.pda)
   }
@@ -64,5 +81,5 @@ imbalance.metrics<-data.frame(shape.yule,colles.yule,p.coless.t.y.less,p.coless.
   return(imbalance.metrics)
 }
 
-imbalance.metrics<-imbalance_metrics(e.trees,1000)
+imbalance.metrics<-imbalance_metrics(bird.sl,10)
 round(imbalance.metrics,3)

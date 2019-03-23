0017c525cab236afbddd8b14ac8fb31779edced8
Francisco Henao Diaz <franciscohenaodiaz@dhcp-206-87-222-15.ubcsecure.wireless.ubc.ca>
2019-03-22 17:31:41 -0700

    to slice simulated trees from the cluster

---
+++ code/sim_tree_slicing.R
@@ -0,0 +1,30 @@
+library(phytools)
+library(parallel)
+
+ntips<-c(100, 1000, 10000, 50000, 100000)
+strees<-mcmapply(pbtree, n = ntips, scale = 1 , SIMPLIFY = F, mc.cores = 10)
+saveRDS(strees, "strees.rds")
+
+s.trees<-readRDS("Slicing/strees/strees.rds")
+
+library(castor)
+ages2cut<-seq(from=.2, to=max(branching.times(s.trees[[1]])), by=0.02)
+stree1.sl<-slice_tree_ages(s.trees[[1]],ages2cut[1:length(ages2cut)])
+
+ages2cut<-seq(from=.2, to=max(branching.times(s.trees[[2]])), by=0.02)
+stree2.sl<-slice_tree_ages(s.trees[[2]],ages2cut[1:length(ages2cut)])
+
+ages2cut<-seq(from=.2, to=max(branching.times(s.trees[[3]])), by=0.02)
+stree3.sl<-slice_tree_ages(s.trees[[3]],ages2cut[1:length(ages2cut)])
+
+ages2cut<-seq(from=.2, to=max(branching.times(s.trees[[4]])), by=0.02)
+stree4.sl<-slice_tree_ages(s.trees[[4]],ages2cut[1:length(ages2cut)])
+
+ages2cut<-seq(from=.2, to=max(branching.times(s.trees[[5]])), by=0.02)
+stree5.sl<-slice_tree_ages(s.trees[[5]],ages2cut[1:length(ages2cut)])
+
+saveRDS(stree1.sl,"Slicing/simtrees/s100/stree1.sl.rds")
+saveRDS(stree2.sl,"Slicing/simtrees/s1k/stree2.sl.rds")
+saveRDS(stree3.sl,"Slicing/simtrees/s10k/stree3.sl.rds")
+saveRDS(stree4.sl,"Slicing/simtrees/s50k/stree4.sl.rds")
+saveRDS(stree5.sl,"Slicing/simtrees/s100k/stree5.sl.rds")

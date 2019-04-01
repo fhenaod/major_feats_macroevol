73154c8c9db2982973210b84cfc4c38ba96cf061
Francisco Henao Diaz <franciscohenaodiaz@dhcp-206-87-219-77.ubcsecure.wireless.ubc.ca>
2019-04-01 10:17:16 -0700

    plot title updated

--- code/sim_tree_slicing.R
+++ code/sim_tree_slicing.R
@@ -27,8 +27,18 @@ stree3.sl<-slice_tree_ages(s.trees[[3]],ages2cut[1:length(ages2cut)])
 ages2cut<-seq(from=.3, to=max(branching.times(s.trees[[4]])), by=0.02)
 stree4.sl<-slice_tree_ages(s.trees[[4]],ages2cut[1:length(ages2cut)])
 
+scale_edges<-function(phylo.list,old_edge,new_edge){
+  for(i in 1:length(stree4.sl)){
+    phylo.list[[i]]$edge.length[phylo.list[[i]]$edge.length<old_edge] <-new_edge
+    phylo.list[[i]]<-castor:::extend_tree_to_height(phylo.list[[i]])$tree
+  }
+  return(phylo.list)
+}
+stree4.sl<-scale_edges(stree4.sl,1e-6, 1e-6)
+
 ages2cut<-seq(from=.3, to=max(branching.times(s.trees[[5]])), by=0.02)
 stree5.sl<-slice_tree_ages(s.trees[[5]],ages2cut[1:length(ages2cut)])
+stree5.sl<-scale_edges(stree5.sl,1e-6, 1e-6)
 
 saveRDS(stree1.sl,"Slicing/simtrees/s100/stree1.sl.rds")
 saveRDS(stree2.sl,"Slicing/simtrees/s1k/stree2.sl.rds")
@@ -42,8 +52,21 @@ s1k_sl_stat<-readRDS("Slicing/simtrees/s1k/s1k_sum_stats.rds")
 s1k_sl_stat$taxon<-"s1k"
 s10k_sl_stat<-readRDS("Slicing/simtrees/s10k/s10k_sum_stats.rds")
 s10k_sl_stat$taxon<-"s10k"
-s50k_sl_stat<-readRDS("Slicing/simtrees/s50k/")
 
 s50k_sl_stat<-readRDS("Slicing/simtrees/s50k/")
+s100k_sl_stat<-readRDS("Slicing/simtrees/s100k/")
 
 sum_stats<-rbind(s100_sl_stat, s1k_sl_stat, s10k_sl_stat)
+
+library(plotly)
+plot_ly(sum_stats, x = ~log(principal_eigenvalue), y = ~asymmetry, z = ~peakedness,
+        type = "scatter3d", mode = "markers",
+        marker = list(symbol = 'circle', sizemode = 'area', 
+                      color = ~tree.max.age, size = ~ntips,
+                      colorbar = list(title = 'Clade age (Myr)'), colorscale='Viridis', reversescale = T)) %>%
+  layout(
+    title = "Sim Trees",
+    scene = list( xaxis = list(title = "Ln Principal eigenvalue"),
+                  yaxis = list(title = "Asymmetry"),
+                  zaxis = list(title = "Peakedness"))
+  )

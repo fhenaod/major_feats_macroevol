4c458bb54eab3bf162607ef4463097ea361d5f5e
Francisco Henao Diaz <franciscohenaodiaz@dhcp-206-87-219-77.ubcsecure.wireless.ubc.ca>
2019-04-01 10:16:49 -0700

    list of non-NULL tyrees and at least 4 tips

--- code/bd_simtrees.R
+++ code/bd_simtrees.R
@@ -10,11 +10,19 @@ pars<-list(birth_rate_factor = lambda,
            death_rate_factor = mu)
 
 bd_sim_tr<-list()
-for(i in 1:1000){
-  bd_sim_tr[[i]]<-generate_random_tree(pars, max_time = age)$tree
+t<-c()
+for(i in 1:100){
+  t<-generate_random_tree(pars, max_time = age)$tree
+  if (!is.null(t)){
+    if (Ntip(t) >= 4) bd_sim_tr[[i]]<-t
+  } else { 
+    i = i-1
+  }	
 }
+
+bd_sim_tr<-bd_sim_tr[sapply(bd_sim_tr, function(x) !is.null(x)==TRUE)]
 saveRDS(bd_sim_tr,"bd_sim_trees.rds")
 
-sapply(bd_sim_tr, ape::Ntip)
+sapply(bd_sim_tr, Ntip)
 sapply(bd_sim_tr, is.binary)
 sapply(bd_sim_tr, is.ultrametric)

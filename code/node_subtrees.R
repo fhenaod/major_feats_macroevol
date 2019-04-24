23e1a8b00257796f7dc332b06ab076bcaa6a50bc
Francisco Henao Diaz <franciscohenaodiaz@Franciscos-MacBook-Air.local>
2019-04-23 18:07:54 -0700

    Class rank added, code organization

--- code/node_subtrees.R
+++ code/node_subtrees.R
@@ -1,11 +1,15 @@
 library(phytools)
 library(parallel)
 
-# Subseting #####
-orders<-unique(seed_tre_new$node.label)[grep("ales",unique(seed_tre_new$node.label))] # orders
+
+# Subseting trees by node labels #####
+classes<-unique(tree$node.label)[grep("ales",unique(tree$node.label))] # Class
+
+orders<-unique(tree$node.label)[grep("ales",unique(tree$node.label))] # Order
 orders<-orders[-13]
-families<-unique(seed_tre_new$node.label)[grep("ceae",unique(seed_tre_new$node.label))] # families
+
+families<-unique(tree$node.label)[grep("ceae",unique(tree$node.label))] # Family
 families<-families[-9]
 
-seed_ord<-mclapply(orders, function(x) extract.clade(phy = seed_tre_new, node = x), mc.cores = 2)
+seed_ord<-mclapply(orders, function(x) extract.clade(phy = tree, node = x), mc.cores = 2)
 saveRDS(seed_ords, "node_subtrees/seed_ord_trees.rds")

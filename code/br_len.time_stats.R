efd62cb8b6e5bb1ca0c723691fe4bd98b120e05f
Francisco Henao Diaz <franciscohenaodiaz@Franciscos-MacBook-Air.local>
2019-05-16 19:18:04 -0700

    add: min branch length

--- code/br_len.time_stats.R
+++ code/br_len.time_stats.R
@@ -1,16 +1,18 @@
 library(ape)
 br_len.t_stats=function(trees){
   df<-data.frame()
+  br.len_min<-c()
   br.len_mean<-c()
   br.t_mean<-c()
   for (i in 1:length(trees)){
     tree<-trees[[i]]
+    br.len_min[i]<-min(tree$edge.length)
     br.len_mean[i]<-mean(tree$edge.length)
     br.t_mean[i]<-mean(branching.times(tree))
   }
-  df<-round(data.frame(br.len_mean, br.t_mean),3)
+  df<-data.frame(br.len_min, br.len_mean, br.t_mean)
   return(df)
 }
-branch_stats<-br_len.t_stats(squa.sl)
+branch_stats<-br_len.t_stats(x.sl)
 
-saveRDS(branch_stats,"Slicing/branch_stats/squa_branch_stats.rds")
+saveRDS(branch_stats,"Slicing/branch_stats/x_branch_stats.rds")

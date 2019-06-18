49574a0c8b83dcdd0d7279951ce0d4a25147ac5f
Francisco Henao Diaz <franciscohenaodiaz@Franciscos-MacBook-Air.local>
2019-06-18 16:28:40 -0700

    fit distributions to data

---
+++ code/fit_dens_distr.R
@@ -0,0 +1,43 @@
+library(fitdistrplus)
+library(actuar)
+
+plotdist(subset(sum_stats, !is.na(shape.yule))$shape.yule, histo = TRUE, demp = TRUE)
+plotdist(subset(sum_stats, !is.na(shape.pda))$shape.pda, histo = TRUE, demp = TRUE)
+plotdist(subset(sum_stats, !is.na(gamma.stat))$gamma.stat, histo = TRUE, demp = TRUE)
+
+descdist(subset(sum_stats, !is.na(shape.yule))$shape.yule, discrete = F, boot = 1000)
+descdist(subset(sum_stats, !is.na(shape.pda))$shape.pda, discrete = F, boot = 1000)
+descdist(subset(sum_stats, !is.na(gamma.stat))$gamma.stat, discrete = F, boot = 1000)
+
+#z-score standarization [0,1]
+x<-subset(sum_stats, !is.na(shape.pda))$shape.pda
+x_scaled<-((x-mean(x))/(sd(x)))
+x_scaled[x_scaled>2]<-(2)
+x_scaled[x_scaled<(-2)]<-(-2)
+x_scaled<-(x_scaled/5)
+x_scaled<-(x_scaled+.5)
+
+#
+fit_b<-fitdist(x_scaled, "beta")
+fit_w<-fitdist(x_scaled, "weibull")
+fit_g<-fitdist(x_scaled, "gamma")
+fit_ln<-fitdist(x_scaled, "lnorm")
+fit_ll<-fitdist(x_scaled, "llogis", start = list(shape = 1, scale = 500))
+fit_P<-fitdist(x_scaled, "pareto", start = list(shape = 1, scale = 500))
+fit_B<-fitdist(x_scaled, "burr", lower = c(0,0,0), start = list(shape1 = 1, shape2 = 1, rate = 1))
+
+gofstat(list(fit_b,fit_w, fit_g, fit_ln, fit_ll, fit_P, fit_B), fitnames = c("Beta", "Weibull", "Gamma", "lnorm", "llogis", "Pareto", "Burr"))
+
+par(mfrow = c(1,1))
+cdfcomp(list(fit_b, fit_g, fit_B), xlogscale = TRUE, ylogscale = TRUE,
+        legendtext = c("Beta", "Gamma", "Burr"), lwd = 2)
+plot.legend<-c("Beta", "Gamma", "Burr")
+denscomp(list(fit_b, fit_g, fit_B), legendtext = plot.legend, xlab = "Shape (Yule)", ylab = "Count", main = "")
+
+par(mfrow = c(2,2))
+plot.legend<-c("Beta", "Gamma", "Burr")
+denscomp(list(fit_b, fit_g, fit_B), legendtext = plot.legend, xlab = "Shape (Yule)", ylab = "Count", main = "")
+cdfcomp(list(fit_b, fit_g, fit_B), legendtext = plot.legend)
+qqcomp(list(fit_b, fit_g, fit_B), legendtext = plot.legend)
+ppcomp(list(fit_b, fit_g, fit_B), legendtext = plot.legend)
+

141b5bb2c736cf78564cb922ecc760a5a451886c
Francisco <fhenaod@zoology.ubc.ca>
2021-03-14 19:49:59 -0700

    + result names to load, custom 3D-scatter plot depending on tree metrics (yule, pda, rpanda) or tree conditions (ntip, sampling)

--- code/scatter3D_gif.R
+++ code/scatter3D_gif.R
@@ -4,6 +4,21 @@ library(plot3D)
 library(animation)
 library(RColorBrewer)
 
+# dataframes ####
+sl_sum_stats   # sliced concensus trees
+rank_sum_stats # rank sampling consensus trees
+fract_sum_node # self-similar sampling by nodes consensus trees
+fract_sum_edg %>% group_by(s_age) %>% sample_n(200, replace = F) # self-similar sampling by edges consensus trees
+
+all_1k_sum  # 1k posterior trees
+all_10_slic # sliced 10 posterior trees
+all_10_rank # rank sampled 10 posterior trees
+all_10_rando %>% group_by(s_age) %>% sample_n(200, replace = F) # self-similar sampled 10 posterior trees
+
+# data frame to plot
+df2plot <- all_10_rando %>% group_by(s_age) %>% sample_n(200, replace = F)
+
+# plot pars ####
 # addalpha()
 addalpha <- function(colors, alpha = 1.0) {
   r <- col2rgb(colors, alpha = T)
@@ -30,34 +45,99 @@ colorRampPaletteAlpha <- function(colors, n = 32, interpolate='linear') {
   return(cr)
 }
 
-rotate <-360
 par(xpd = TRUE)
-cols<-colorRampPalette(brewer.pal(11,"Spectral"))(100)
-pal<-addalpha(cols,.5)
+cols <- colorRampPalette(brewer.pal(11,"Spectral"))(100)
+pal <- addalpha(cols, .6)
 
+# rpanda ####
+rotate <-360
 saveGIF({
-  
   while (rotate>0)
   {
-    rotate<-rotate-5 # change to 1 and run locally for rotation animation 
+    rotate<-rotate-2 # change to 1 and run locally for rotation animation 
     
-    scatter3D(log(sum_stats$principal_eigenvalue), 
-              sum_stats$asymmetry, 
-              log(sum_stats$peakedness),  
-              colvar = sum_stats$tree.max.age,
+    scatter3D(log(df2plot$principal_eigenvalue), 
+              df2plot$asymmetry, 
+              log(df2plot$peakedness),  
+              colvar = df2plot$tree.max.age,
+              #colvar = df2plot$rel_age,
+              #colvar = df2plot$orig_brake_age_f,
               col = pal,
               xlab = "Expansion", 
               ylab = "Tippiness",
               zlab = "Branch length heterogeneity",
               clab = "Clade age (Myr)",
+              #clab = "Relative age",
+              #clab = "Breaking age",
               theta = rotate,
               phi = 30,
-              pch = 16,
-              cex = log(sum_stats$ntips),
+              #pch = as.numeric(factor(df2plot$taxon))+10,
+              pch = as.numeric(factor(df2plot$taxon))+12,
+              #pch = as.numeric(factor(df2plot$taxon))+14,
+              cex = log(df2plot$ntips)/2,
               bty = "b2",
               add = FALSE)
   }
-}, interval = 0.1, movie.name = "slicing_3d.gif", ani.width = 800, ani.height = 600)
+}, interval = 0.2, movie.name = "all_10post_edge_rpanda.gif", ani.width = 800, ani.height = 600)
 
 # Save ggplot at high resolution
 ggsave("Rplot.png", width = 20, height = 20, units = "cm", dpi = 320)
+
+# shape stats ####
+# Yule
+rotate <-360
+saveGIF({
+  
+  while (rotate>0)
+  {
+    rotate<-rotate-2 # change to 1 and run locally for rotation animation 
+    
+    scatter3D(log(df2plot$shape.yule), 
+              df2plot$colles.yule, 
+              log(df2plot$sackin.yule),  
+              #colvar = df2plot$tree.max.age,
+              colvar = df2plot$rel_age,
+              col = pal,
+              xlab = "Shape (Yule)", 
+              ylab = "Colless (Yule)",
+              zlab = "Sackin (Yule)",
+              clab = "Clade age (Myr)",
+              #clab = "Relative age",
+              theta = rotate,
+              phi = 30,
+              pch = as.numeric(factor(df2plot$taxon))+10,
+              #pch = as.numeric(factor(df2plot$taxon))+14,
+              cex = log(df2plot$ntips)/2,
+              bty = "b2",
+              add = FALSE)
+  }
+}, interval = 0.2, movie.name = "all_10post_rank_yule.gif", ani.width = 800, ani.height = 600)
+
+# PDA
+rotate <-360
+saveGIF({
+  
+  while (rotate>0)
+  {
+    rotate<-rotate-2 # change to 1 and run locally for rotation animation 
+    
+    scatter3D(log(df2plot$shape.pda), 
+              df2plot$colles.pda, 
+              log(df2plot$sackin.pda),  
+              #colvar = df2plot$tree.max.age,
+              colvar = df2plot$rel_age,
+              col = pal,
+              xlab = "Shape (PDA)", 
+              ylab = "Colless (PDA)",
+              zlab = "Sackin (PDA)",
+              clab = "Clade age (Myr)",
+              #clab = "Relative age",
+              theta = rotate,
+              phi = 30,
+              pch = as.numeric(factor(df2plot$taxon))+10,
+              #pch = as.numeric(factor(df2plot$taxon))+14,
+              cex = log(df2plot$ntips)/2,
+              bty = "b2",
+              add = FALSE)
+  }
+}, interval = 0.2, movie.name = "all_10post_rank_pda.gif", ani.width = 800, ani.height = 600)

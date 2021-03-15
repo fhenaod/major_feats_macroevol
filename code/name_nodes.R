4f2112ef973790e225f5d280c5e9bf5132df5137
Francisco <fhenaod@zoology.ubc.ca>
2021-03-14 19:48:44 -0700

    + node naming based on taxonomy and tree, uses drop tip instead mrca for higher ranks

--- code/name_nodes.R
+++ code/name_nodes.R
@@ -1,7 +1,11 @@
 library(phytools)
+library(tidyverse)
 library(taxize)
 library(castor)
 
+tree <- TE2016 # select the tree  
+clad <- "fern"
+  
 # get genus names ####
 names_sp<-strsplit(tree$tip.label, "_")
 get_genus=function(names_sp){
@@ -14,7 +18,7 @@ get_genus=function(names_sp){
 genera<-unique(get_genus(names_sp))
 tree$tip.label <- gsub("_", " ", tree$tip.label)
 
-# Name genera nodes ####
+# Name genera nodes, for non-monospecific genus ####
 name_genus_nodes=function(tree, names2nodes){
   for(i in 1:length(names2nodes)){
     tips<-tree$tip.label[grep(paste0("\\b", names2nodes[i] ,"\\b"), tree$tip.label)]
@@ -29,13 +33,13 @@ name_genus_nodes=function(tree, names2nodes){
 }
 tre_g_noded<-name_genus_nodes(tree, genera)
 
-# Name higher rank nodes on a genus tree, based on taxonomy table ####
-m<-read.csv("taxonomy/_tax.csv", header = T, row.names = 1)
+# Name higher rank nodes on a genus-tip-tree, based on taxonomy table ####
+m <- read.csv(paste0("taxonomy/", clad,"_tax.csv"), header = T, row.names = 1)
 name_htaxa_nod_gen_tip=function(tree, m){
   fams<-unique(m$family)
   for(i in 1:length(fams)){
-    fam_g<-as.character(m[grep(fams[i],m$family),]$genus)
-    if(length(fam_g)<=2) {next()}
+    fam_g<-as.character(m[grep(fams[i], m$family),]$genus)
+    if(length(fam_g)<2) {next()}
     mrca_node<-get_mrca_of_set(tree, fam_g)
     if(mrca_node!=1) {
       tree$node.label[mrca_node-Ntip(tree)]<-as.character(fams[i])
@@ -62,33 +66,39 @@ name_htaxa_nod_gen_tip=function(tree, m){
   
   return(tree)
 }
-tre_noded<-name_htaxa_nod_gen_tip(tre_g_noded, m)
+tre_noded <- name_htaxa_nod_gen_tip(tre_g_noded, m)
 
 # Name higher rank nodes based on taxonomy table ####
-m<-read.csv("taxonomy/_tax.csv", header = T, row.names = 1)
+m <- read.csv(paste0("taxonomy/", clad,"_tax.csv"), header = T, row.names = 1)
 name_htaxa_nodes=function(tree, m){
-  fams<-unique(m$family)
+  tr_nam_tab <- data.frame(spp = tree$tip.label, 
+                           genus = get_genus(names_sp)) %>% 
+    left_join(m, by = c("genus"="genus"))
+  
+  fams<-unique(tr_nam_tab$family)
   for(i in 1:length(fams)){
-    fam_g<-as.character(m[grep(fams[i],m$family),]$genus)
-    mrca_node<-get_mrca_of_set(tree, fam_g)
+    fam_t <- filter(tr_nam_tab, family == fams[i]) %>% pull(spp)
+    if(length(fam_t)==1){next()}
+    mrca_node<-get_mrca_of_set(tree, fam_t)
     if(mrca_node!=1) {
       tree$node.label[mrca_node-Ntip(tree)]<-as.character(fams[i])
     } 
   }
   
-  ords<-unique(m$order)
+  ords<-unique(tr_nam_tab$order)
   for(j in 1:length(ords)){
-    ord_f<-as.character(m[grep(ords[j],m$order),]$family)
-    mrca_node<-get_mrca_of_set(tree, ord_f)
+    ord_t<-filter(tr_nam_tab, order == ords[j]) %>% pull(spp)
+    if(length(ord_t)==1){next()}
+    mrca_node<-get_mrca_of_set(tree, ord_t)
     if(mrca_node!=1) {
       tree$node.label[mrca_node-Ntip(tree)]<-as.character(ords[j])
     } 
   }
   
-  clas<-unique(m$class)
+  clas<-unique(tr_nam_tab$class)
   for(k in 1:length(ords)){
-    cla_o<-as.character(m[grep(clas[k],m$class),]$order)
-    mrca_node<-get_mrca_of_set(tree, cla_o)
+    cla_t<-filter(tr_nam_tab, class == clas[k]) %>% pull(spp)
+    mrca_node<-get_mrca_of_set(tree, cla_t)
     if(mrca_node!=1) {
       tree$node.label[mrca_node-Ntip(tree)]<-as.character(clas[k])
     } 
@@ -96,6 +106,10 @@ name_htaxa_nodes=function(tree, m){
   
   return(tree)
 }
-tre_noded<-name_htaxa_nodes(tre_g_noded, m)
+tre_noded <- name_htaxa_nodes(tre_g_noded, m)
 
-saveRDS(tre_noded,"data_megaPhylos/_noded_tre.rds")
+plot(tre_noded, show.tip.label = F, no.margin = T, type = "fan")
+nodelabels(tre_noded$node.label, frame = "none", col = "red", cex = .65)
+
+saveRDS(tre_noded, paste0("data_megaPhylos/", clad,"_noded_tre.rds"))
+

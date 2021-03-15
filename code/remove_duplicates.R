f4def9a94dda3b91dfd8c32c731bbcba11e36ab0
Francisco <fhenaod@zoology.ubc.ca>
2021-03-14 19:07:27 -0700

    - included on other files

--- code/remove_duplicates.R
+++
@@ -1,9 +0,0 @@
-# Remove duplicate species, subspecies or varieties
-remove_duplicates=function(phylo, names_list){
-  for(i in 1:length(names_list)){
-    t_labs<-extract.clade(phy = phylo, node = names_list[i])$tip.label
-    t_drop<-t_labs[1:length(t_labs)-1]
-    new_tre<-drop.tip(phylo, t_drop)
-}
-return(new_tre)
-}

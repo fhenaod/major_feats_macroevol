ff8d2842cda614f946306fcd2f735066ad086b9e
Francisco <fhenaod@zoology.ubc.ca>
2021-03-14 19:10:05 -0700

    - minor line deletion

--- code/tree_struct.R
+++ code/tree_struct.R
@@ -12,8 +12,6 @@ plot(extract.clade(p, 10), show.node.label = T, cex = .8)
 nodelabels(frame = "none", col = "red", adj = c(1.5,-0.5), cex = 0.8)
 edgelabels(round(extract.clade(p, 10)$edge.length,5), frame = "none", col = "blue", adj = c(1,-0.5), cex = .8)
 
-?ladderize
-
 plot(tre_noded,  edge.width = 2, cex = 0.4, 
       show.tip.label = F, type = "fan")
 nodelabels(tre_noded$node.label,frame = "none", col = "red", adj = c(1.1,-0.4),cex = 0.6)

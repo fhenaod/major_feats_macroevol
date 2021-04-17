6bb51dda686938ebff038fc2a5f881e007568751
Francisco <fhenaod@zoology.ubc.ca>
2021-04-16 19:10:34 -0700

    + sim bd trees based on ntips from henao-diaz pnas collection

--- code/bd_simtrees.R
+++ code/bd_simtrees.R
@@ -26,3 +26,13 @@ saveRDS(bd_sim_tr,"bd_sim_trees.rds")
 sapply(bd_sim_tr, Ntip)
 sapply(bd_sim_tr, is.binary)
 sapply(bd_sim_tr, is.ultrametric)
+
+# sim trees using henao-diaz pnas ntips
+e.trees <- henao_d_trs
+e_trees <- e.trees [sapply(e.trees, Ntip) > 20]
+
+sim_b.7d.5 <- list()
+for(i in 1:length(e_trees)){
+  sim_b.7d.5[[i]] <- TreeSim::sim.bd.taxa(n = sapply(e_trees, Ntip)[i],
+                                          numbsim = 1, lambda = 0.7, mu = 0.5)[[1]]
+}

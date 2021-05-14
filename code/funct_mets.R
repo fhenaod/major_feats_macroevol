7afc8ec2e332ed95fa53c4d399ca70f569d9260c
Francisco <fhenaod@zoology.ubc.ca>
2021-05-14 01:52:13 -0700

    + function to compute splitting beta and its CI by MaxLik

--- code/funct_mets.R
+++ code/funct_mets.R
@@ -246,6 +246,36 @@ imbalance_metrics=function(e.trees, n.mc){
   return(imbalance.metrics)
 }
 
+# from a list of trees get the maximum likelihood estimate of the Beta-splitting model
+# and CI:95% with bootstrap replicates or ML profile
+get_ml_beta=function(trees, n_bootstrap){
+  beta<-c()
+  b_low_ci<-c()
+  b_up_ci<-c()
+  for(i in 1:length(trees)){
+    if(Ntip(trees[[i]])<=2) {
+      beta[i]<-NA
+      b_low_ci[i]<-NA
+      b_up_ci[i]<-NA
+    } else {
+      if(Ntip(trees[[i]])<=50) {
+        ml_beta<-maxlik.betasplit(trees[[i]], confidence.interval = "bootstrap", conf.level = 0.95, size.bootstrap = n_bootstrap)
+        beta[i]<-ml_beta$max_lik
+        b_low_ci[i]<-ml_beta$conf_interval[[1]]
+        b_up_ci[i]<-ml_beta$conf_interval[[2]]
+      } else {
+        ml_beta<-maxlik.betasplit(trees[[i]], confidence.interval = "profile", conf.level = 0.95, size.bootstrap = n_bootstrap)
+        beta[i]<-ml_beta$max_lik
+        b_low_ci[i]<-ml_beta$conf_interval[[1]]
+        b_up_ci[i]<-ml_beta$conf_interval[[2]]
+      }
+    }
+  }
+  df<-data.frame(beta, b_low_ci, b_up_ci) %>% 
+    mutate_if(is.numeric, round, 3)
+  return(df)
+}
+
 extract_spect=function(trees_spectR){
   principal_eigenvalue<-c()
   asymmetry<-c()
